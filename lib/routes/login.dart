acf37fec29a30176da8062bfb02f150838beab85
vincentzyc <vincentzyc@qq.com>
2023-12-06 09:17:42 +0800

    🎉init: init

--- lib/routes/login.dart
+++ lib/routes/login.dart
@@ -10,20 +10,20 @@ class LoginRoute extends StatefulWidget {
 }
 
 class _LoginRouteState extends State<LoginRoute> {
-  TextEditingController _unameController = new TextEditingController();
-  TextEditingController _pwdController = new TextEditingController();
+  TextEditingController _unameController = TextEditingController();
+  TextEditingController _pwdController = TextEditingController();
   bool pwdShow = false;
-  // final GlobalKey _formKey = GlobalKey<FormState>();
+  GlobalKey _formKey = GlobalKey<FormState>();
   bool _nameAutoFocus = true;
 
-  @override
+  // @override
   // void initState() {
-  //   // 自动填充上次登录的用户名，填充后将焦点定位到密码输入框
-  //   _unameController.text = Global.profile.lastLogin ?? "";
-  //   if (_unameController.text.isNotEmpty) {
-  //     _nameAutoFocus = false;
-  //   }
-  //   super.initState();
+  // 自动填充上次登录的用户名，填充后将焦点定位到密码输入框
+  // _unameController.text = Global.profile.lastLogin ?? "";
+  // if (_unameController.text.isNotEmpty) {
+  //   _nameAutoFocus = false;
+  // }
+  // super.initState();
   // }
 
   @override
@@ -34,7 +34,7 @@ class _LoginRouteState extends State<LoginRoute> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          // key: _formKey,
+          key: _formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
             children: <Widget>[
@@ -93,34 +93,33 @@ class _LoginRouteState extends State<LoginRoute> {
 
   void _onLogin() async {
     // 先验证各个表单字段是否合法
-    print('登录');
-    Navigator.of(context).pop();
-    // if ((_formKey.currentState as FormState).validate()) {
-    //   // 验证通过
-    //   if ((_formKey.currentState as FormState).validate()) {
-    //     // showLoading(context);
-    //     User? user;
-    //     try {
-    //       user = await Git(context)
-    //           .login(_unameController.text, _pwdController.text);
-    //       // 因为登录页返回后，首页会build，所以我们传false，更新user后不触发更新
-    //       Provider.of<UserModel>(context, listen: false).user = user;
-    //     } on DioError catch (e) {
-    //       //登录失败则提示
-    //       if (e.response?.statusCode == 401) {
-    //         showToast('账号或密码不正确');
-    //       } else {
-    //         showToast(e.toString());
-    //       }
-    //     } finally {
-    //       // 隐藏loading框
-    //       Navigator.of(context).pop();
-    //     }
-    //     //登录成功则返回
-    //     if (user != null) {
-    //       Navigator.of(context).pop();
-    //     }
-    //   }
-    // }
+    if ((_formKey.currentState as FormState).validate()) {
+      //  验证通过
+      Navigator.of(context).pop();
+      //   if ((_formKey.currentState as FormState).validate()) {
+      //     // showLoading(context);
+      //     User? user;
+      //     try {
+      //       user = await Git(context)
+      //           .login(_unameController.text, _pwdController.text);
+      //       // 因为登录页返回后，首页会build，所以我们传false，更新user后不触发更新
+      //       Provider.of<UserModel>(context, listen: false).user = user;
+      //     } on DioError catch (e) {
+      //       //登录失败则提示
+      //       if (e.response?.statusCode == 401) {
+      //         showToast('账号或密码不正确');
+      //       } else {
+      //         showToast(e.toString());
+      //       }
+      //     } finally {
+      //       // 隐藏loading框
+      //       Navigator.of(context).pop();
+      //     }
+      //     //登录成功则返回
+      //     if (user != null) {
+      //       Navigator.of(context).pop();
+      //     }
+      //   }
+    }
   }
 }
