87c994d19302946a2c8794fe6f5f731ccdacd774
vincentzyc <vincentzyc@qq.com>
2023-12-13 16:42:53 +0800

    🎉init: init

--- lib/states/profile_change_notifier.dart
+++ lib/states/profile_change_notifier.dart
@@ -15,17 +15,17 @@ import 'package:flutter/material.dart';
 import '../models/index.dart';
 
 class UserModel extends ChangeNotifier {
-  User? get user => _profile.user;
+  User? get user => null;
 
   // APP是否登录(如果有用户信息，则证明登录过)
   bool get isLogin => user != null;
 
   //用户信息发生变化，更新用户信息并通知依赖它的子孙Widgets更新
-  set user(User? user) {
-    if (user?.login != _profile.user?.login) {
-      _profile.lastLogin = _profile.user?.login;
-      _profile.user = user;
-      notifyListeners();
-    }
-  }
+  // set user(User? user) {
+  //   if (user?.login != _profile.user?.login) {
+  //     _profile.lastLogin = _profile.user?.login;
+  //     _profile.user = user;
+  //     notifyListeners();
+  //   }
+  // }
 }
