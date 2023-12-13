32f0c9a58ccd49a5befd89f08e4c81ba282f9cb5
vincentzyc <vincentzyc@qq.com>
2023-12-13 18:10:43 +0800

    🎉init: init

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -7,22 +7,22 @@ class User {
   User();
 
   late String login;
-  late String avatar_url;
+  late String avatarUrl;
   late String type;
   String? name;
-  String? company;
-  String? blog;
+  // String? company;
+  // String? blog;
   String? location;
   String? email;
-  bool? hireable;
-  String? bio;
-  late num public_repos;
-  late num followers;
-  late num following;
-  late String created_at;
-  late String updated_at;
-  late num total_private_repos;
-  late num owned_private_repos;
+  // bool? hireable;
+  // String? bio;
+  // late num public_repos;
+  // late num followers;
+  // late num following;
+  // late String created_at;
+  // late String updated_at;
+  // late num total_private_repos;
+  // late num owned_private_repos;
   
   factory User.fromJson(Map<String,dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
