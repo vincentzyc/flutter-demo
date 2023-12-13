f5eab7cfea1f08b1bdf0fc353ddca686709b9cfd
vincentzyc <vincentzyc@qq.com>
2023-12-13 09:51:48 +0800

    🎉init: init

---
+++ lib/states/profile_change_notifier.dart
@@ -0,0 +1,31 @@
+// import '../index.dart';
+
+// class ProfileChangeNotifier extends ChangeNotifier {
+  // Profile get _profile => Global.profile;
+
+  // @override
+  // void notifyListeners() {
+  //   Global.saveProfile(); //保存Profile变更
+  //   super.notifyListeners(); //通知依赖的Widget更新
+  // }
+// }
+
+import 'package:flutter/material.dart';
+
+import '../models/index.dart';
+
+class UserModel extends ChangeNotifier {
+  User? get user => _profile.user;
+
+  // APP是否登录(如果有用户信息，则证明登录过)
+  bool get isLogin => user != null;
+
+  //用户信息发生变化，更新用户信息并通知依赖它的子孙Widgets更新
+  set user(User? user) {
+    if (user?.login != _profile.user?.login) {
+      _profile.lastLogin = _profile.user?.login;
+      _profile.user = user;
+      notifyListeners();
+    }
+  }
+}
