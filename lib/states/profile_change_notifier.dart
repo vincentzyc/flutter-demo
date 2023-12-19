068ced653cbbe36af4cdbb6161999295845957d2
vincentzyc <vincentzyc@qq.com>
2023-12-19 09:08:36 +0800

    🎉init: init

--- lib/states/profile_change_notifier.dart
+++ lib/states/profile_change_notifier.dart
@@ -12,25 +12,33 @@
 
 import 'package:flutter/material.dart';
 
-import '../models/index.dart';
+// class UserModel extends ChangeNotifier {
+//   User? user;
+//   String userName = '';
 
-class UserModel extends ChangeNotifier {
-  User? user;
-  String userName = '';
+//   // APP是否登录(如果有用户信息，则证明登录过)
+//   bool get isLogin => userName != '';
 
-  // APP是否登录(如果有用户信息，则证明登录过)
-  bool get isLogin => user != null;
+//   //用户信息发生变化，更新用户信息并通知依赖它的子孙Widgets更新
+//   void setUser(User? user) {
+//     user = user;
+//     // 通知监听器（订阅者），重新构建InheritedProvider， 更新状态。
+//     notifyListeners();
+//   }
 
-  //用户信息发生变化，更新用户信息并通知依赖它的子孙Widgets更新
-  void setUser(User? user) {
-    user = user;
-    // 通知监听器（订阅者），重新构建InheritedProvider， 更新状态。
-    notifyListeners();
-  }
+//   void setUserName(String userName) {
+//     userName = userName;
+//     // 通知监听器（订阅者），重新构建InheritedProvider， 更新状态。
+//     notifyListeners();
+//   }
+// }
+class UserProvider with ChangeNotifier {
+  String _username = '';
 
-  void setUserName(String userName) {
-    userName = userName;
-    // 通知监听器（订阅者），重新构建InheritedProvider， 更新状态。
+  String get username => _username;
+
+  void setUsername(String username) {
+    _username = username;
     notifyListeners();
   }
 }
