a60ce47d34557c15e103c28922a07294046bcf4e
vincentzyc <vincentzyc@qq.com>
2023-12-25 18:58:29 +0800

    🎉init: init

---
+++ lib/theme/mColors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class mColors {
+  static const Color text_blue = Color(0xFF4490f6);
+  static const Color divider_line_bg = Color(0xFFe6e6e6);
+  static const Color light_grey = Color(0xFFf0f0f0);
+  static const Color light_green = Color(0xFFdbfffa);
+  static const Color hint_color = Color(0xFF969696);
+  static const Color grey_button = Color(0xFF979797);
+  static const Color text_black = Color(0xFF323232);
+  static const Color text_grey = Color(0xFFA9A9A9);
+  static const Color light_blue = Color(0xFFe4f2ff);
+  static const Color text_red = Color(0xFFff7070);
+  static const Color progress_line = Color(0xFF77b1e8);
+  static const Color auto_buy = Color(0xFFfff2e9);
+  static const Color orange = Color(0xFFffb785);
+}
