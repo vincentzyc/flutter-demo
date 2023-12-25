62687f5718314c74d53f8926eb165840fb2098be
vincentzyc <vincentzyc@qq.com>
2023-12-25 18:58:30 +0800

    🎉init: init

--- lib/routes/my.dart
+++ lib/routes/my.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_demo/models/user.dart';
+import '../theme/mColors.dart';
 import 'package:provider/provider.dart';
 
 import '../states/index.dart';
@@ -25,110 +26,67 @@ class _MyRouteState extends State<MyRoute> {
     return Scaffold(
       appBar: AppBar(title: const Text('个人中心')),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                  autofocus: _nameAutoFocus,
-                  controller: _unameController,
-                  decoration: const InputDecoration(
-                    labelText: '用户名',
-                    hintText: '用户名',
-                    prefixIcon: Icon(Icons.person),
+          padding: const EdgeInsets.all(16.0),
+          child: Stack(children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              color: mColors.text_blue,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 60, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      const Image(
+                        image: AssetImage(("assets/image/test.jpg")),
+                        width: 50,
+                        height: 50,
+                      ),
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.topLeft,
+                        margin: const EdgeInsets.only(left: 5),
+                        child: const Column(children: [
+                          SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              "您好，欢迎您",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              "连接你的网络世界",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                          )
+                        ]),
+                      ))
+                    ],
                   ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.deny(RegExp('[\\s]')),
-                  ],
-                  // 校验用户名（不能为空）
-                  validator: (v) {
-                    return v == null || v.trim().isNotEmpty ? null : '用户名不能为空';
-                  }),
-              TextFormField(
-                controller: _pwdController,
-                autofocus: !_nameAutoFocus,
-                decoration: InputDecoration(
-                    labelText: '密码',
-                    hintText: '密码',
-                    prefixIcon: const Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                          pwdShow ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          pwdShow = !pwdShow;
-                        });
-                      },
-                    )),
-                obscureText: !pwdShow,
-                inputFormatters: [
-                  FilteringTextInputFormatter.deny(RegExp('[\\s]')),
                 ],
-                //校验密码（不能为空）
-                validator: (v) {
-                  // return v == null || v.trim().isNotEmpty ? null : '密码必填';
-                  return v!.trim().length > 5 ? null : "密码不能少于6位";
-                },
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 25),
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints.expand(height: 55.0),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.blue),
-                        foregroundColor:
-                            MaterialStateProperty.all(Colors.white),
-                        // 文字大小
-                        textStyle: MaterialStateProperty.all(
-                          const TextStyle(fontSize: 20),
-                        )),
-                    child: const Text("登录"),
-                    onPressed: _onLogin,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            )
+          ])),
     );
   }
 
   void _onLogin() async {
-    // 先验证各个表单字段是否合法
+    // 验证表单字段
     if ((_formKey.currentState as FormState).validate()) {
-      // UserModel userModel = Provider.of<UserModel>(context, listen: false);
       final userProvider = Provider.of<UserProvider>(context, listen: false);
-      //  验证通过
       userProvider.setUsername(_unameController.text);
       Navigator.of(context).pop();
-      //   if ((_formKey.currentState as FormState).validate()) {
-      //     // showLoading(context);
-      //     User? user;
-      //     try {
-      //       user = await Git(context)
-      //           .login(_unameController.text, _pwdController.text);
-      //       // 因为登录页返回后，首页会build，所以我们传false，更新user后不触发更新
-      //       Provider.of<UserModel>(context, listen: false).user = user;
-      //     } on DioError catch (e) {
-      //       //登录失败则提示
-      //       if (e.response?.statusCode == 401) {
-      //         showToast('账号或密码不正确');
-      //       } else {
-      //         showToast(e.toString());
-      //       }
-      //     } finally {
-      //       // 隐藏loading框
-      //       Navigator.of(context).pop();
-      //     }
-      //     //登录成功则返回
-      //     if (user != null) {
-      //       Navigator.of(context).pop();
-      //     }
-      //   }
     }
   }
 }
