d74436d5b740eb27391e6ee8eac6cc1b82bb1d4f
vincentzyc <vincentzyc@qq.com>
2023-12-26 10:38:16 +0800

    🎉init: init

--- lib/theme/mColors.dart
+++ lib/theme/mColors.dart
@@ -8,7 +8,7 @@ class mColors {
   static const Color hint_color = Color(0xFF969696);
   static const Color grey_button = Color(0xFF979797);
   static const Color text_black = Color(0xFF323232);
-  static const Color text_grey = Color(0xFFA9A9A9);
+  static const Color text_gray = Color(0xFFA9A9A9);
   static const Color light_blue = Color(0xFFe4f2ff);
   static const Color text_red = Color(0xFFff7070);
   static const Color progress_line = Color(0xFF77b1e8);
