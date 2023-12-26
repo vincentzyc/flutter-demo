6fc1c33d0845505c94e4ddd6b8a5a5e77936d1d6
vincentzyc <vincentzyc@qq.com>
2023-12-26 11:14:01 +0800

    🎉init: init

--- lib/widgets/my_setting.dart
+++ lib/widgets/my_setting.dart
@@ -1,34 +1,40 @@
 import 'package:flutter/material.dart';
-import '../theme/mColors.dart';
+// import '../theme/mColors.dart';
 
 class MySetting extends StatelessWidget {
-  const MySetting({super.key});
+  MySetting({super.key});
+
+  final List<Map<String, dynamic>> settings = [
+    {'title': '个人资料', 'icon': Icons.person},
+    {'title': '隐私设置', 'icon': Icons.lock},
+    {'title': '通知管理', 'icon': Icons.notifications},
+    {'title': '帮助与反馈', 'icon': Icons.help},
+    {'title': '关于我们', 'icon': Icons.info},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Flex(
-          direction: Axis.horizontal,
-          children: [
-            Expanded(
-              flex: 1,
-              child: ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text('设置'),
-                trailing: const Icon(Icons.arrow_forward_ios),
-                onTap: () {
-                  showToast(context, '这是提示文字哦~');
-                },
-              ),
-            ),
-          ],
-        ),
-        // 添加分割线
-        const Divider(
-          height: 3,
-          indent: 0,
-          color: mColors.text_gray,
+        ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: settings.length,
+          separatorBuilder: (BuildContext context, int index) => const Divider(
+            height: 1,
+            indent: 0,
+            color: Color(0xFFE0E0E0),
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Icon(settings[index]['icon']),
+              title: Text(settings[index]['title']),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                showToast(context, '这是提示文字哦~ 当前索引：${index + 1}');
+              },
+            );
+          },
         ),
       ],
     );
@@ -36,6 +42,7 @@ class MySetting extends StatelessWidget {
 
 // 新增一个toast弹出提示函数
   void showToast(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
     final snackBar = SnackBar(
       content: Text(msg),
       duration: const Duration(seconds: 3),
