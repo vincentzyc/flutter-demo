443e8f5fe5f776e2cfd1a10d5996bee790d705dd
vincentzyc <vincentzyc@qq.com>
2023-12-28 12:24:14 +0800

    🎉init: init

---
+++ lib/routes/about-us.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_demo/models/user.dart';
+import '../theme/mColors.dart';
+import 'package:provider/provider.dart';
+
+import '../states/index.dart';
+import '../widgets/index.dart';
+
+// 新增关于我们页面
+class AboutUsRoute extends StatefulWidget {
+  const AboutUsRoute({super.key});
+
+  @override
+  _AboutUsRouteState createState() => _AboutUsRouteState();
+}
+
+class _AboutUsRouteState extends State<AboutUsRoute> {
+  @override
+  Widget build(BuildContext context) {
+    // var gm = GmLocalizations.of(context);
+    return Scaffold(
+        appBar: AppBar(title: const Text('关于我们')),
+        body: Padding(
+            padding: const EdgeInsets.only(top: 0),
+            child: Column(
+              children: <Widget>[],
+            )));
+  }
+}
