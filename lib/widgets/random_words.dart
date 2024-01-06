9ecebcd6af7665d801c1a29f00451e5d6b199dcd
vincentzyc <vincentzyc@qq.com>
2024-01-06 09:07:07 +0800

    🎉init: init

--- lib/widgets/random_words.dart
+++ lib/widgets/random_words.dart
@@ -1,17 +1,27 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
+final dio = Dio();
+
+void getHttp() async {
+  final response = await dio.get('https://dart.dev');
+  print(response);
+}
 
 class RandomWords extends StatelessWidget {
   const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-   // 生成随机字符串
+    // 生成随机字符串
     final wordPair = WordPair.random();
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Text(wordPair.toString()),
+      child: GestureDetector(
+        onTap: () => getHttp(), // 执行getHttp操作
+        child: Text(wordPair.toString()),
+      ),
     );
   }
 }
