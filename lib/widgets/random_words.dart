847e49e9ae3d1722b3d24e278c279eb153cd716f
vincentzyc <vincentzyc@qq.com>
2024-01-09 22:42:29 +0800

    🎉init: init

--- lib/widgets/random_words.dart
+++ lib/widgets/random_words.dart
@@ -1,32 +1,54 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_demo/models/ipaddress.dart';
+import 'package:flutter_demo/utils/toast_util.dart';
 // import 'dart:convert';
 
 final dio = Dio();
 
-void getHttp() async {
+Future<String> getHttp() async {
   // final response = await dio.get('https://dart.dev');
 
   // var response = await dio.get('https://pv.sohu.com/cityjson');
-  var response = await dio.get('https://restapi.amap.com/v3/ip?parameters');
-  print(response);
+  Response response = await dio.get('https://www.ip.cn/api/index?ip&type=0');
 
+  IpAddress ipAddress = ipAddressFromJson(response.toString());
+
+  mToast(ipAddress.address);
+  return ipAddress.address;
 }
 
-class RandomWords extends StatelessWidget {
+class RandomWords extends StatefulWidget {
   const RandomWords({super.key});
 
   @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  String ipCity = '';
+
+  @override
   Widget build(BuildContext context) {
-    // 生成随机字符串
     final wordPair = WordPair.random();
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: GestureDetector(
-        onTap: () => getHttp(), // 执行getHttp操作
-        child: Text(wordPair.toString()),
+        onTap: () {
+          _updateIpCity(); // 执行异步操作，并在操作完成后更新状态
+        },
+        child: Text(ipCity != '' ? ipCity : wordPair.toString()),
       ),
     );
+  }
+
+  Future<void> _updateIpCity() async {
+    String newIpCity = await getHttp(); // 执行异步操作
+    setState(() {
+      ipCity = newIpCity; // 同步更新状态
+      print(ipCity);
+    });
   }
 }
