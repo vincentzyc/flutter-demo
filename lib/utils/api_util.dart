56bf3636a4b3bcace2de73889f393fd441d1fee9
vincentzyc <vincentzyc@qq.com>
2024-01-29 19:15:01 +0800

    🎉init: init

--- lib/utils/api_util.dart
+++ lib/utils/api_util.dart
@@ -1,1 +1,20 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+final dio = Dio();
+
 const generateToken = "https://token.jetmobo.com/service/token/generate";
+
+Future<String> getToken(data) async {
+  // 获取token
+  var requestBody = {
+    "data": jsonEncode(data), // 待加密数据，json字符串格式
+    "expireSeconds": 3600, // 过期时间，单位秒
+  };
+
+  Response response = await dio.post(generateToken, data: requestBody);
+
+  var token = response.data['data']['token'];
+  return token;
+}
