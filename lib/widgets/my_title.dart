1f033d39b52c79641dc579bed90537e7f1f0d838
vincentzyc <vincentzyc@qq.com>
2024-02-03 10:29:54 +0800

    🎉init: init

--- lib/widgets/my_title.dart
+++ lib/widgets/my_title.dart
@@ -3,7 +3,7 @@ import '../theme/mColors.dart';
 
 class MyTitle extends StatelessWidget {
   const MyTitle({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Stack(children: <Widget>[
@@ -28,29 +28,30 @@ class MyTitle extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                    child: Container(
-                  alignment: Alignment.topLeft,
-                  margin: const EdgeInsets.only(left: 10),
-                  child: const Column(children: [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        "您好，欢迎您",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 19,
-                            fontWeight: FontWeight.bold),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.only(left: 10),
+                    child: const Column(children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "您好，欢迎您",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        "这是个人中心页面",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
-                      ),
-                    )
-                  ]),
-                ))
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "这是个人中心页面",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      )
+                    ]),
+                  ),
+                ),
               ],
             ),
           ],
